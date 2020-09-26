464c9dc3913e17e982b6fa30b49a62519c634362
tkim30201 <63823948+tkim30201@users.noreply.github.com>
2020-09-26 11:00:22 -0400

    added prompts for lowercase and uppercase letters

--- script.js
+++ script.js
@@ -15,6 +15,8 @@ generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
   var characterAmount = prompt("Enter desired password length.(Must be between 8 and 128 characters)")
+  var includeUpper = prompt("Would you like to include uppercase letters?")
+  var includeLower = prompt("Would you like to include lowercase letters?")
   var includeNumbers = prompt("Would you like to include numbers?")
   var includeSymbols = prompt("Would you like to include symbols?")
 }
